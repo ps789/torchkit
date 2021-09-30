7bbe19c1b0f709097417bd4277f7b7f1e7fb79a3
phillipsi <>
2021-09-30 19:37:15 -0400

    NAF structures for quantile

--- torchkit/flows.py
+++ torchkit/flows.py
@@ -561,9 +561,257 @@ class IAF_DDSF(BaseFlow):
 
         assert out_dim == 1, 'last dsf out dim should be 1'
         return h[:,:,0], lgd[:,:,0,0].sum(1) + logdet, context
+class IAF_DDSF_Quantile_Early_Layers(BaseFlow):
+
+    def __init__(self, dim, hid_dim, context_dim, num_layers,
+                 activation=nn.ELU(), fixed_order=False,
+                 num_ds_dim=4, num_ds_layers=1, num_ds_multiplier=3):
+        super(IAF_DDSF_Quantile_Early_Layers, self).__init__()
+
+        self.dim = dim
+        self.context_dim = context_dim
+        self.num_ds_dim = num_ds_dim
+        self.num_ds_layers = num_ds_layers
+
+        if type(dim) is int:
+            self.mdl = iaf_modules.cMADE(
+                    dim, hid_dim, context_dim, num_layers,
+                    num_ds_multiplier*(hid_dim//dim)*num_ds_layers,
+                    activation, fixed_order)
+
+        num_dsparams = 0
+        for i in range(num_ds_layers):
+            if i == 0:
+                in_dim = 1
+            else:
+                in_dim = num_ds_dim
+            if i == num_ds_layers-1:
+                out_dim = 1
+            else:
+                out_dim = num_ds_dim
+
+            u_dim = in_dim
+            w_dim = num_ds_dim
+            a_dim = b_dim = num_ds_dim
+            num_dsparams += u_dim + w_dim + a_dim + b_dim
+
+            self.add_module('sf{}'.format(i),
+                            DenseSigmoidFlow(in_dim,
+                                             num_ds_dim,
+                                             out_dim))
+        if type(dim) is int:
+            self.out_to_dsparams = nn.Conv1d(
+                num_ds_multiplier*(hid_dim//dim)*num_ds_layers,
+                num_dsparams, 1)
+        else:
+            self.out_to_dsparams = nn.Conv1d(
+                num_ds_multiplier*(hid_dim/dim[0])*num_ds_layers,
+                num_dsparams, 1)
+
+
+        self.reset_parameters()
+
+
+    def reset_parameters(self):
+        self.out_to_dsparams.weight.data.uniform_(-0.001, 0.001)
+        self.out_to_dsparams.bias.data.uniform_(0.0, 0.0)
+
+
+    def forward(self, inputs):
+        x, logdet, context, alphas = inputs
+        out, _ = self.mdl((x, context))
+        out = out.permute(0,2,1)
+        dsparams = self.out_to_dsparams(out).permute(0,2,1)
+
+
+        start = 0
+
+        h = x.view(x.size(0),-1)[:,:,None]
+        alphas_h = alphas.view(alphas.size(0),-1)[:,:,None]
+        n = x.size(0)
+        dim = self.dim if type(self.dim) is int else self.dim[0]
+        lgd = Variable(torch.from_numpy(
+            np.zeros((n, dim, 1, 1)).astype('float32')))
+        if self.out_to_dsparams.weight.is_cuda:
+            lgd = lgd.cuda()
+        for i in range(self.num_ds_layers):
+            if i == 0:
+                in_dim = 1
+            else:
+                in_dim = self.num_ds_dim
+            if i == self.num_ds_layers-1:
+                out_dim = 1
+            else:
+                out_dim = self.num_ds_dim
+
+            u_dim = in_dim
+            w_dim = self.num_ds_dim
+            a_dim = b_dim = self.num_ds_dim
+            end = start + u_dim + w_dim + a_dim + b_dim
+
+            params = dsparams[:,:,start:end]
+            h, lgd = getattr(self,'sf{}'.format(i))(h, lgd, params, alphas_h)
+            start = end
+
+        assert out_dim == 1, 'last dsf out dim should be 1'
+        return h[:,:,0], lgd[:,:,0,0].sum(1) + logdet, context, alphas
+class IAF_DDSF_Quantile(BaseFlow):
+
+    def __init__(self, dim, hid_dim, context_dim, num_layers,
+                 activation=nn.ELU(), fixed_order=False,
+                 num_ds_dim=4, num_ds_layers=1, num_ds_multiplier=3):
+        super(IAF_DDSF_Quantile, self).__init__()
+
+        self.dim = dim
+        self.context_dim = context_dim
+        self.num_ds_dim = num_ds_dim
+        self.num_ds_layers = num_ds_layers
+
+        if type(dim) is int:
+            self.mdl = iaf_modules.cMADE(
+                    dim, hid_dim, context_dim, num_layers,
+                    num_ds_multiplier*(hid_dim//dim)*num_ds_layers,
+                    activation, fixed_order)
+
+        num_dsparams = 0
+        for i in range(num_ds_layers):
+            if i == 0:
+                in_dim = 1
+            else:
+                in_dim = num_ds_dim
+            if i == num_ds_layers-1:
+                out_dim = 1
+            else:
+                out_dim = num_ds_dim
+
+            u_dim = in_dim
+            w_dim = num_ds_dim
+            a_dim = b_dim = num_ds_dim
+            num_dsparams += u_dim + w_dim + a_dim + b_dim
+
+            self.add_module('sf{}'.format(i),
+                            DenseSigmoidFlowQuantile(in_dim,
+                                             num_ds_dim,
+                                             out_dim))
+        if type(dim) is int:
+            self.out_to_dsparams = nn.Conv1d(
+                num_ds_multiplier*(hid_dim//dim)*num_ds_layers,
+                num_dsparams, 1)
+        else:
+            self.out_to_dsparams = nn.Conv1d(
+                num_ds_multiplier*(hid_dim/dim[0])*num_ds_layers,
+                num_dsparams, 1)
+
+
+        self.reset_parameters()
+
+
+    def reset_parameters(self):
+        self.out_to_dsparams.weight.data.uniform_(-0.001, 0.001)
+        self.out_to_dsparams.bias.data.uniform_(0.0, 0.0)
+
+
+    def forward(self, inputs):
+        x, logdet, context, alphas = inputs
+        out, _ = self.mdl((x, context))
+        out = out.permute(0,2,1)
+        dsparams = self.out_to_dsparams(out).permute(0,2,1)
+
+
+        start = 0
+
+        h = x.view(x.size(0),-1)[:,:,None]
+        alphas_h = alphas.view(alphas.size(0),-1)[:,:,None]
+        n = x.size(0)
+        dim = self.dim if type(self.dim) is int else self.dim[0]
+        lgd = Variable(torch.from_numpy(
+            np.zeros((n, dim, 1, 1)).astype('float32')))
+        if self.out_to_dsparams.weight.is_cuda:
+            lgd = lgd.cuda()
+        for i in range(self.num_ds_layers):
+            if i == 0:
+                in_dim = 1
+            else:
+                in_dim = self.num_ds_dim
+            if i == self.num_ds_layers-1:
+                out_dim = 1
+            else:
+                out_dim = self.num_ds_dim
+
+            u_dim = in_dim
+            w_dim = self.num_ds_dim
+            a_dim = b_dim = self.num_ds_dim
+            end = start + u_dim + w_dim + a_dim + b_dim
+
+            params = dsparams[:,:,start:end]
+            h, lgd = getattr(self,'sf{}'.format(i))(h, lgd, params, alphas_h)
+            start = end
+
+        assert out_dim == 1, 'last dsf out dim should be 1'
+        return h[:,:,0], lgd[:,:,0,0].sum(1) + logdet, context, alphas
 
 
 
+class DenseSigmoidFlowQuantile(BaseFlow):
+
+    def __init__(self, in_dim, hidden_dim, out_dim):
+        super(DenseSigmoidFlowQuantile, self).__init__()
+        self.in_dim = in_dim
+        self.hidden_dim = hidden_dim
+        self.out_dim = out_dim
+
+        self.act_a = lambda x: nn_.softplus(x)
+        self.act_b = lambda x: x
+        self.act_w = lambda x: nn_.softmax(x,dim=3)
+        self.act_u = lambda x: nn_.softmax(x,dim=3)
+
+        self.u_ = Parameter(torch.Tensor(hidden_dim, in_dim))
+        self.w_ = Parameter(torch.Tensor(out_dim, hidden_dim))
+
+        self.reset_parameters()
+
+    def reset_parameters(self):
+        self.u_.data.uniform_(-0.001, 0.001)
+        self.w_.data.uniform_(-0.001, 0.001)
+
+
+    def forward(self, x, logdet, dsparams, alphas_h):
+        inv = np.log(np.exp(1-nn_.delta)-1)
+        ndim = self.hidden_dim
+        pre_u = self.u_[None,None,:,:]+dsparams[:,:,-self.in_dim:][:,:,None,:]
+        pre_w = self.w_[None,None,:,:]+dsparams[:,:,2*ndim:3*ndim][:,:,None,:]
+        a = self.act_a(dsparams[:,:,0*ndim:1*ndim]+inv)
+        b = self.act_b(dsparams[:,:,1*ndim:2*ndim])
+        w = self.act_w(pre_w)
+        u = self.act_u(pre_u)
+
+        pre_sigm = torch.sum(u * a[:,:,:,None] * torch.erfinv(2*alphas_h[:,:,None,:]-1), 3) + b
+        sigm = torch.sigmoid(pre_sigm)
+        x_pre = torch.sum(w*sigm[:,:,None,:], dim=3)
+        x_pre_clipped = x_pre * (1-nn_.delta) + nn_.delta * 0.5
+        x_ = log(x_pre_clipped) - log(1-x_pre_clipped)
+        xnew = x_
+
+        logj = F.log_softmax(pre_w, dim=3) + \
+            nn_.logsigmoid(pre_sigm[:,:,None,:]) + \
+            nn_.logsigmoid(-pre_sigm[:,:,None,:]) + log(a[:,:,None,:])
+        # n, d, d2, dh
+
+        logj = logj[:,:,:,:,None] + F.log_softmax(pre_u, dim=3)[:,:,None,:,:]
+        # n, d, d2, dh, d1
+
+        logj = utils.log_sum_exp(logj,3).sum(3)
+        # n, d, d2, d1
+
+        logdet_ = logj + np.log(1-nn_.delta) - \
+            (log(x_pre_clipped) + log(-x_pre_clipped+1))[:,:,:,None]
+
+
+        logdet = utils.log_sum_exp(
+            logdet_[:,:,:,:,None] + logdet[:,:,None,:,:], 3).sum(3)
+        # n, d, d2, d1, d0 -> n, d, d2, d0
+
+        return xnew, logdet
 class DenseSigmoidFlow(BaseFlow):
 
     def __init__(self, in_dim, hidden_dim, out_dim):
@@ -587,7 +835,7 @@ class DenseSigmoidFlow(BaseFlow):
         self.w_.data.uniform_(-0.001, 0.001)
 
 
-    def forward(self, x, logdet, dsparams):
+    def forward(self, x, logdet, dsparams, alphas):
         inv = np.log(np.exp(1-nn_.delta)-1)
         ndim = self.hidden_dim
         pre_u = self.u_[None,None,:,:]+dsparams[:,:,-self.in_dim:][:,:,None,:]
@@ -625,7 +873,6 @@ class DenseSigmoidFlow(BaseFlow):
         # n, d, d2, d1, d0 -> n, d, d2, d0
 
         return xnew, logdet
-
 
 class FlipFlow(BaseFlow):
 
